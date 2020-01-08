29628d84c81cb22701ae337c5bcb9a9e24836670
maunabesanika <maunabesanika@student.uns.ac.id>
2020-01-08 15:52:32 +0700

    2nd commit

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -6,9 +6,31 @@
             <img src="/storage/{{$post->image}}" class="w-100">
         </div>
         <div class="col-4">
-            <h2>{{ $post->user->username }}</h2>
+            <div>
+                <div class="d-flex align-items-center">
+                    <div class="pr-3">
+                    <img src="{{$post->user->profile->profileImage()}}" style="max-width: 50px" class="rounded-circle w-100">
+                    </div>
+                    <div>
+                         <div class="font-weight-bold">
+                           <a href="/profile/{{$post->user->id}}">
+                            <span class="text-dark">{{$post->user->username}}</span>
+                            </a>
+                             <a href="#" class="pl-2">Follow</a>
+                         </div>
+                    </div>
+                </div>
 
-            <p>{{ $post->caption }}</p>
+                <hr>
+
+            <p>
+                <span class="font-weight-bold">
+                    <a href="/profile/{{ $post->user->id }}">
+                        <span class="text-dark">{{ $post->user->username }} </span>
+                    </a>
+                </span>{{ $post->caption }}
+            </p>
+            </div>
         </div>
     </div>
 </div>
--- resources/views/profiles/index.blade.php
+++ resources/views/profiles/index.blade.php
@@ -4,11 +4,16 @@
 <div class="container">
     <div class="row">
         <div class="col-3 p-5">
-            <img src="https://i.pinimg.com/originals/59/ba/5d/59ba5d55ebf2b13c6a351b2d36f89dba.jpg" style="height: 170px" class="rounded-circle">
+            <img src="{{$user->profile->profileImage()}}" class="rounded-circle w-100">
         </div>
         <div class="col-9 pt-5">
             <div class="d-flex justify-content-between align-items-baseline">
-                <h1>{{ $user -> username }}</h1>
+
+                <div class="d-flex align-items-center pb-3">
+                    <div class="h4">{{ $user -> username }}</div>
+
+                    <follow-button user-id="{{$user -> id}}"></follow-button>
+                </div>
 
                 @can('update',$user->profile)
                     <a href="/p/create">Add New Post</a>
