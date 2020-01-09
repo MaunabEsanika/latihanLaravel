3ee4d0dfe419e2160f8c5ffb4996bde050c4c0f0
maunabesanika <maunabesanika@student.uns.ac.id>
2020-01-09 15:27:01 +0700

    Calculating Followers Count and Following Count Laravel Telescope Showing Posts from Profiles The User Is Following Pagination with Eloquent N + 1 Problem & Solution

--- app/Profile.php
+++ app/Profile.php
@@ -13,6 +13,11 @@ class Profile extends Model
         $imagePath = ($this -> image) ?  $this->image : 'profile/TgJtQMcqGAx7DVLOtCS2IeSBfRe0HpTukTQ6FSkq.png';
         return '/storage/' . $imagePath;
     }
+
+    public function followers(){
+        return $this->belongsToMany(User::class);
+    }
+
     public function user(){
         return $this->belongsTo(User::class);
     }
