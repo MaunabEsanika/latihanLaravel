de1857c76c45303ee85551c71bd5248809d1d835
maunabesanika <maunabesanika@student.uns.ac.id>
2020-01-10 15:17:32 +0700

    fix bug and error

--- app/Http/Controllers/ProfilesController.php
+++ app/Http/Controllers/ProfilesController.php
@@ -10,35 +10,36 @@ class ProfilesController extends Controller
     public function index(User $user)
     {
         $follows = (auth()->user())? auth()->user()->following->contains($user->id): false;
+        $postCount = Cache::remember(
+            'count.posts'. $user->id,
+            now()->addSeconds(5),
+            function()use ($user){
+                return $user->posts->count();
+            });
 
-        return view('profiles.index', compact('user', 'follows'));
+        $followersCount = Cache::remember(
+            'count.posts'. $user->id,
+            now()->addSeconds(5),
+            function()use ($user){
+                return $user->profile->followers->count();;
+            });
+
+        $followingCount = Cache::remember(
+            'count.posts'. $user->id,
+            now()->addSeconds(5),
+            function()use ($user){
+                return $user->following->count();
+            });
+
+        return view('profiles.index', compact('user', 'follows',  'postCount', 'followersCount', 'followingCount'));
     }
 
     public function edit(User $user){
        $this->authorize('update', $user->profile);
 
-       $postCount = Cache::remember(
-           'count.posts'. $user->id,
-           now()->addSeconds(5),
-           function()use ($user){
-               return $user->posts->count();
-           });
 
-       $followersCount = Cache::remember(
-           'count.posts'. $user->id,
-           now()->addSeconds(5),
-           function()use ($user){
-               return $user->profile->followers->count();;
-           });
 
-       $followingCount = Cache::remember(
-           'count.posts'. $user->id,
-           now()->addSeconds(5),
-           function()use ($user){
-               return $user->following->count();;
-           });
-
-       return view('profiles.edit', compact('user', 'postCount', 'followersCount', 'followingCount'));
+       return view('profiles.edit', compact('user'));
     }
 
     public function update(User $user){
