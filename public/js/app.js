b14c17e24b3806f3d52e178e3a03e38f0f8b9190
hakivin <hakivinamana@student.uns.ac.id>
2020-01-10 15:39:30 +0700

    change app name

--- public/js/app.js
+++ public/js/app.js
@@ -49611,8 +49611,8 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! C:\xampp\htdocs\latihanLaravel\resources\js\app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! C:\xampp\htdocs\latihanLaravel\resources\sass\app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! C:\Users\W10\PhpstormProjects\Intagram\resources\js\app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! C:\Users\W10\PhpstormProjects\Intagram\resources\sass\app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
